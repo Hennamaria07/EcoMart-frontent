85e6826a9e6d0b8404b1a8eadc9f104a7ff11906
Henna Maria <hennamaria2001@gmail.com>
2024-04-06 11:12:49 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+        <h1 className='py-32'>Home</h1>
+    </Layout>
+  )
+}
+
+export default Home
