3ceaca44fe16e486ea19c630c0bbc3ecf4dec6b3
Henna Maria <hennamaria2001@gmail.com>
2024-04-06 13:10:42 +0530

    helmet and footer were added

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
-const Layout = ({children}) => {
+const Layout = ({ children, title, description, keyword, author, viewport}) => {
   return (
     <div>
+        <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+          <meta name="keywords" content={keyword} />
+            <meta name="author" content={author} />
+              <meta name="viewport" content={viewport} />
+              </Helmet>
+              </HelmetProvider>
         <Header />
         <main>
             {children}
         </main>
+        <Footer />
     </div>
   )
 }
